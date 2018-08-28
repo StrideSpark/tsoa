541285b08457c746485a2e3ebcd25ba75435f3b3
Frank Prößdorf <frank@naa.li>
2018-08-28 07:02:25 +0300

    Also generate OpenAPI 3.0.0 spec when 3.0.1 given

--- src/module/generate-swagger-spec.ts
+++ src/module/generate-swagger-spec.ts
@@ -20,6 +20,41 @@ export const generateSwaggerSpec = async (
         metadata = new MetadataGenerator(config.entryFile, compilerOptions, ignorePaths).Generate();
     }
     let spec: Swagger.Spec;
+
+    // if (args.yaml) {
+    //     config.swagger.yaml = args.yaml;
+    //   }
+    //   if (args.json) {
+    //     config.swagger.yaml = false;
+    //   }
+
+    //   const compilerOptions = validateCompilerOptions(config.compilerOptions);
+    //   const swaggerConfig = validateSwaggerConfig(config.swagger);
+    //   const metadata = new MetadataGenerator(swaggerConfig.entryFile, compilerOptions, config.ignore).Generate();
+    //   let spec: Swagger.Spec;
+    //   if (config.swagger.version && config.swagger.version.split('.')[0] === '3') {
+    //     spec = new SpecGenerator3(metadata, config.swagger).GetSpec();
+    //   } else {
+    //     spec = new SpecGenerator2(metadata, config.swagger).GetSpec();
+    //   }
+
+    //   const exists = fs.existsSync(swaggerConfig.outputDirectory);
+    //   if (!exists) {
+    //     fs.mkdirSync(swaggerConfig.outputDirectory);
+    //   }
+    //   let data = JSON.stringify(spec, null, '\t');
+    //   if (config.swagger.yaml) {
+    //     data = YAML.stringify(JSON.parse(data), 10);
+    //   }
+    //   const ext = config.swagger.yaml ? 'yaml' : 'json';
+
+    //   fs.writeFileSync(`${swaggerConfig.outputDirectory}/swagger.${ext}`, data, { encoding: 'utf8' });
+    // } catch (err) {
+    //   // tslint:disable-next-line:no-console
+    //   console.error('Generate swagger error.\n', err);
+    //   process.exit(1);
+    // }
+
     // if (config.swagger.version && config.swagger.version.split('.')[0] === '3') {
     spec = new SpecGenerator3(metadata, config).GetSpec();
     // } else {
